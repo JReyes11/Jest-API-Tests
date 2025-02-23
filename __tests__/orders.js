e9202755c27028d46262709c36a3a1ac2b176c96
JReyes11 <jf.reyes925@gmail.com>
2025-02-22 22:22:24 -0600

    class methods

--- __tests__/orders.js
+++ __tests__/orders.js
@@ -1,115 +1,108 @@
 import "regenerator-runtime/runtime";
 require("dotenv").config();
-import { getConfig, postConfig, makeRequest } from "../support/apiRequests";
-import { endpoints } from "../fixtures/parameters";
-import { userInfo } from "../fixtures/users";
-import { assertOrdersPayload, mockOrderObject, updateOrder } from "../support/helper";
+import api from "../support/apiRequests";
+import userFixtures from "../fixtures/users";
+import helper from "../support/helper";
 
 describe("API Test Cases: Orders", function () {
-  let endpointInfo, userData, baseUrl
+  let userData;
   beforeEach(async function () {
-    endpointInfo = await endpoints();
-    baseUrl = endpointInfo.baseUrl
-    userData = await userInfo();
+    userData = await userFixtures.userIdAndEmail();
   });
 
-  test("Look up existing order; Assert Items array and total price.", async function () {
-    // Get a random user from the userInfo fixtures file.
-    const userObj = userData.data[Math.floor(Math.random() * userData.data.length)]    
-    const endpoint = `${baseUrl}${endpointInfo.orders}`; // define the orders endpoint.
-    const config = await getConfig(endpoint); // Retrieve axios config object.
-    const req = await makeRequest(config); // Make the API request
-    expect(req.status).toBe(200); // Assert 200 response
-    const orderData  = req.data.filter(s => s.email == userObj.email)[0] // filter orders for user.
-    await assertOrdersPayload(orderData, userObj) // Perform assertions (response vs userFixture)    
+  test("Existing order; Assert Items array and total price.", async function () {
+    // Lookup order for random user; Assert payload response.
+    const userObj = userData[Math.floor(Math.random() * userData.length)];
+    const req = await api.getOrders();
+    expect(req.status).toBe(200);
+    const orderData = req.data.filter((s) => s.email == userObj.email)[0];
+    await helper.assertOrdersPayload(orderData, userObj);
   });
 
   test("Create Order; Assert order returned in search results.", async function () {
-    // retrieve a random existing user. 
-    const userEndpoint = `${baseUrl}${endpointInfo.users}`
-    const config = await new getConfig(userEndpoint); // create axios config object.
-    const req = await new makeRequest(config); // Make the API request
-    expect(req.status).toBe(200); // Assert 201 response    
-    const randomUserObj = req.data[Math.floor(Math.random() * req.data.length)]
+    // Get random user.
+    const req = await api.getUsers();
+    expect(req.status).toBe(200);
+    const randomUserObj = req.data[Math.floor(Math.random() * req.data.length)];
 
     // Mock the order object and make post request.
-    const mockOrderObj = await mockOrderObject(randomUserObj)    
-    const endpoint = `${baseUrl}${endpointInfo.orders}`;        
-    const conf = await new postConfig(endpoint, mockOrderObj);
-    const request = await new makeRequest(conf); 
-    expect(request.status).toBe(201); 
+    const mockOrderObj = await helper.mockOrderRequestObject(randomUserObj);
+    const request = await api.postOrders(mockOrderObj);
+    expect(request.status).toBe(201);
 
     // Retrieve the created order and assert response object parity with the mocked Object.
-    const confObj = await new getConfig(endpoint); 
-    const getOrders = await new makeRequest(confObj)
-    expect(getOrders.status).toBe(200);    
-    const recentlyCreatedOrder = getOrders.data.filter(s => s.id == mockOrderObj.id)
-    expect(recentlyCreatedOrder.length).toBeGreaterThan(0)
-    
-    // perform assertions on the first object    
-    expect(recentlyCreatedOrder[0].email).toEqual(randomUserObj.email)
-    expect(recentlyCreatedOrder[0].items.length).toEqual(recentlyCreatedOrder[0].totalItems)
-    const sumOfItems = recentlyCreatedOrder[0].items.map(e => e.price).reduce((a,b) => {
-      return a + b
-    })    
-    expect(recentlyCreatedOrder[0].totalPrice).toEqual(sumOfItems)    
-  })
+    const orderReq = await api.getOrders();
+    expect(orderReq.status).toBe(200);
+    const recentlyCreatedOrder = orderReq.data.filter(
+      (s) => s.id == mockOrderObj.id
+    );
+    expect(recentlyCreatedOrder.length).toBeGreaterThan(0);
+
+    // perform assertions on the first object
+    expect(recentlyCreatedOrder[0].email).toEqual(randomUserObj.email);
+    expect(recentlyCreatedOrder[0].items.length).toEqual(
+      recentlyCreatedOrder[0].totalItems
+    );
+    const sumOfItems = recentlyCreatedOrder[0].items
+      .map((e) => e.price)
+      .reduce((a, b) => {
+        return a + b;
+      });    
+    expect(recentlyCreatedOrder[0].totalPrice).toEqual(sumOfItems);
+  });
 
   test("Update Order; Remove item from order; Assert persistence.", async function () {
-    // Select a random existing order.
-    const endpoint = `${baseUrl}${endpointInfo.orders}`;        
-    const config = await getConfig(endpoint); 
-    const req = await makeRequest(config); 
-    const moreThanOneItem = req.data.filter(s => s.totalItems > 1)   
-    const randomOrder = moreThanOneItem[Math.floor(Math.random() * moreThanOneItem.length)]       
-    
-    // remove one item; update totalItems and totalPrice of the order.    
-    const firstItemPrice = randomOrder.items[0].price
-    randomOrder.totalItems = randomOrder.totalItems -1
-    randomOrder.totalPrice = randomOrder.totalPrice - firstItemPrice   
-    randomOrder.items.splice(0,1)
-    
+    // Get random order.
+    const req = await api.getOrders();
+    const moreThanOneItem = req.data.filter((s) => s.totalItems > 1);
+    const randomOrder =
+      moreThanOneItem[Math.floor(Math.random() * moreThanOneItem.length)];
+
+    // remove one item; update totalItems and totalPrice of the order.
+    const firstItemPrice = randomOrder.items[0].price;
+    randomOrder.totalItems = randomOrder.totalItems - 1;
+    randomOrder.totalPrice = randomOrder.totalPrice - firstItemPrice;
+    randomOrder.items.splice(0, 1);
+
     // update the order.
-    const updateTheOrder = await updateOrder(randomOrder)
-    expect(updateTheOrder.status).toBe(200)
-    
+    const updateTheOrder = await api.updateOrder(randomOrder);
+    expect(updateTheOrder.status).toBe(200);
+
     //Assert the response
-    expect(updateTheOrder.data.id).toBe(randomOrder.id)
-    expect(updateTheOrder.data.items.length).toBe(randomOrder.totalItems)
-    expect(updateTheOrder.data.totalItems).toBe(randomOrder.totalItems)
-    expect(updateTheOrder.data.totalPrice).toBe(randomOrder.totalPrice)    
-  })
+    expect(updateTheOrder.data.id).toBe(randomOrder.id);
+    expect(updateTheOrder.data.items.length).toBe(randomOrder.totalItems);
+    expect(updateTheOrder.data.totalItems).toBe(randomOrder.totalItems);
+    expect(updateTheOrder.data.totalPrice).toBe(randomOrder.totalPrice);
+  });
 
   test("Update Order; Add item to Order; Assert persistence.", async function () {
     // get a random product
-    const endpoint = `${baseUrl}${endpointInfo.products}`;        
-    const config = await getConfig(endpoint); 
-    const req = await makeRequest(config);
-    const randomProduct = req.data[Math.floor(Math.random() * req.data.length-1)]
-        
+    const req = await api.getProducts();
+    const randomProduct =
+      req.data[Math.floor(Math.random() * req.data.length - 1)];
+
     // get a random order
-    const ordersEndpoint = `${baseUrl}${endpointInfo.orders}`;        
-    const orderConfig = await getConfig(ordersEndpoint); 
-    const reqOrders = await makeRequest(orderConfig);
-    const data = reqOrders.data
-    const randomOrder = data[Math.floor(Math.random() * reqOrders.data.length-1)]
-    
+    const reqOrders = await api.getOrders();
+    const data = reqOrders.data;
+    const randomOrder =
+      data[Math.floor(Math.random() * reqOrders.data.length - 1)];
+
     // push random product info to items array.
     randomOrder.items.push({
-      id: randomProduct.id, 
-      department: randomProduct.department, 
-      price: randomProduct.price
-    })
+      id: randomProduct.id,
+      department: randomProduct.department,
+      price: randomProduct.price,
+    });
 
     // update random orders totalItems and Price
-    randomOrder.totalItems = randomOrder.totalItems + 1
-    randomOrder.totalPrice = randomOrder.totalPrice + randomProduct.price
+    randomOrder.totalItems = randomOrder.totalItems + 1;
+    randomOrder.totalPrice = randomOrder.totalPrice + randomProduct.price;
 
-    // Send PUT Request to update the order; Assert response. 
-    const updateTheOrder = await updateOrder(randomOrder)
-    expect(updateTheOrder.status).toBe(200)
-    expect(updateTheOrder.data.items.length).toBe(randomOrder.totalItems)
-    expect(updateTheOrder.data.totalItems).toBe(randomOrder.totalItems)
-    expect(updateTheOrder.data.totalPrice).toBe(randomOrder.totalPrice)
-  })
+    // Update Order and perform assertions.
+    const updateTheOrder = await api.updateOrder(randomOrder);
+    expect(updateTheOrder.status).toBe(200);
+    expect(updateTheOrder.data.items.length).toBe(randomOrder.totalItems);
+    expect(updateTheOrder.data.totalItems).toBe(randomOrder.totalItems);
+    expect(updateTheOrder.data.totalPrice).toBe(randomOrder.totalPrice);
+  });
 });
--- __tests__/users.js
+++ __tests__/users.js
@@ -1,42 +1,27 @@
 import "regenerator-runtime/runtime";
 require("dotenv").config();
-import { getConfig, postConfig, makeRequest } from "../support/apiRequests";
-import { endpoints } from "../fixtures/parameters";
-import { userInfo } from "../fixtures/users";
-import { assertUserPayload, mockNewUserObject } from "../support/helper";
+import api from "../support/apiRequests";
+import userFixtures from "../fixtures/users";
+import helper from "../support/helper";
 
 describe("API Test Cases: Users", function () {
-  let endpointInfo, userData, baseUrl
-  beforeEach(async function () {
-    endpointInfo = await endpoints();
-    baseUrl = endpointInfo.baseUrl
-    userData = await userInfo();
+  beforeEach(async function () {});
 
-  });
-
-  test("GET '/users'; Assert more than 1 record returned; Assert Object Keys.", async function () {
-    // Retrieve userFixture and define endpoint
-    const userObjKeys = Object.keys(userData.data[0])
-    const endpoint = `${baseUrl}${endpointInfo.users}`;      
-    const config = await getConfig(endpoint); // Retrieve axios config object.
-    const req = await makeRequest(config); // Make the API request
+  test("Get random user, Assert expected object keys returned.", async function () {
+    const userKeys = Object.keys(userFixtures.userKeys()); // user Fixture
+    const req = await api.getUsers(); // get users
     expect(req.status).toBe(200); // Assert 200 response
-    expect(req.data.length).toBeGreaterThan(1) // assert more than one record returned.
-    expect(userObjKeys).toEqual(Object.keys(req.data[0]))
+    expect(req.data.length).toBeGreaterThan(1); // assert more than one record returned.
+    expect(userKeys).toEqual(Object.keys(req.data[0])); // asssert response keys.
   });
 
-  test("POST '/users': Create user; Then GET '/users': Verify response object.", async function () {
-    const userObj = await mockNewUserObject() // mock new user object
-    const endpoint = `${baseUrl}${endpointInfo.users}`;        
-    const config = await postConfig(endpoint, userObj); // create axios config object.
-    const req = await makeRequest(config); // Make the API request
-    expect(req.status).toBe(201); // Assert 201 response
-    // Get request to retrieve the created user payload.
-    const userEndpoint = `${baseUrl}${endpointInfo.users}/${userObj.id}`;
-    const conf = await getConfig(userEndpoint)
-    const request = await makeRequest(conf)
-    expect(request.status).toBe(200); // Assert 200 response
-    await assertUserPayload(request.data, userObj)     
+  test("Create user, then confirm user object returned when using id.", async function () {
+    // Create New User, then confirm new user returned when searching via ID.
+    const userObj = await helper.mockNewUserObject();
+    const req = await api.createUser(userObj);
+    expect(req.status).toBe(201);
+    const getUser = await api.getUserById(userObj.id);
+    expect(getUser.status).toBe(200);
+    await helper.assertUserPayload(getUser.data, userObj);
   });
-
 });
