f1a37d13fbf13a7f8a7c5eca580e70997940e4d8
JReyes11 <jf.reyes925@gmail.com>
2025-02-23 22:36:22 -0600

    users and fixtures

--- __tests__/users.js
+++ __tests__/users.js
@@ -16,12 +16,62 @@ describe("API Test Cases: Users", function () {
   });
 
   test("Create user, then confirm user object returned when using id.", async function () {
-    // Create New User, then confirm new user returned when searching via ID.
-    const userObj = await helper.mockNewUserObject();
+    const userObj = await helper.mockNewUserObject();    
     const req = await api.createUser(userObj);
     expect(req.status).toBe(201);
     const getUser = await api.getUserById(userObj.id);
     expect(getUser.status).toBe(200);
     await helper.assertUserPayload(getUser.data, userObj);
   });
+
+  test("Create user, then Delete user; Assert user not found.", async function () {
+    // create the user
+    const userObj = await helper.mockNewUserObject();
+    const req = await api.createUser(userObj);
+    expect(req.status).toBe(201);
+
+    // lookup the user
+    const getUser = await api.getUserById(userObj.id);
+    expect(getUser.status).toBe(200);
+    await helper.assertUserPayload(getUser.data, userObj);
+
+    // delete the user
+    const deleteTheUser = await api.deleteUser(userObj.id);
+    expect(deleteTheUser.status).toBe(200);
+
+    // attempt to lookup the user. Assert 404 status returned.
+    const userLookup = await api.getUserById(userObj.id);
+    expect(userLookup.status).toBe(404);
+  });
+
+  test("Update user address, Assert updates are applied", async function () {
+    // select a random user
+    const users = await api.getUsers()
+    expect(users.status).toBe(200);
+    const selectedUser = users.data[Math.floor(Math.random() * users.data.length)]
+    // collect the users current address information
+    const currentAddress = {
+      id: selectedUser.id,
+      address1: selectedUser.address1, 
+      address2: selectedUser.address2, 
+      city: selectedUser.city, 
+      state: selectedUser.state, 
+      zipCode: selectedUser.zipCode
+    }
+    // update the user object with a new address.
+    const updatedAddress = await helper.updateUserAddressObj(selectedUser)
+    const updateUser = await api.updateUser(updatedAddress)
+    expect(updateUser.status).toBe(200)
+
+    // get the user and confirm the updated address returned in the response. 
+    const findUser = await api.getUserById(selectedUser.id)
+    const response = findUser.data
+    expect(selectedUser.id).toBe(response.id)
+    expect(selectedUser.email).toBe(response.email)
+    expect(selectedUser.full_name).toBe(response.full_name)    
+    expect(currentAddress.address1).not.toEqual(response.address1)
+    expect(currentAddress.city).not.toEqual(response.city)
+    expect(currentAddress.state).not.toEqual(response.state)
+    expect(currentAddress.zipCode).not.toEqual(response.zipCode)
+  })
 });
